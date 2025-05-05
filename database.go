8e39e8b835f2f99347173f8e566376c71f6309fa
bakurvik <v.kurbatskaia@gmail.com>
2025-05-05 10:34:27 +0200

    change SetupDB

--- database.go
+++ database.go
@@ -2,19 +2,37 @@ package common
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func SetupDB(envPath string, testDBEnv string) (*sql.DB, error) {
+const (
+	dbUser     = "DB_USER"
+	dbPassword = "DB_PASSWORD"
+	dbHost     = "DB_HOST"
+	dbPort     = "DB_PORT"
+	dbName     = "DB_NAME"
+)
+
+func SetupDB(envPath string) (*sql.DB, error) {
 	err := godotenv.Load(envPath)
 	if err != nil {
 		return nil, err
 	}
-	dbUrl := os.Getenv(testDBEnv)
+
+	dbUrl := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", os.Getenv(dbUser), os.Getenv(dbPassword), os.Getenv(dbHost), os.Getenv(dbPort), os.Getenv(dbName))
 	return sql.Open("postgres", dbUrl)
+}
+
+func SetupDBByUrl(envPath string, dbUrl string) (*sql.DB, error) {
+	err := godotenv.Load(envPath)
+	if err != nil {
+		return nil, err
+	}
+	return sql.Open("postgres", os.Getenv(dbUrl))
 }
 
 func CloseDB(db *sql.DB) {
