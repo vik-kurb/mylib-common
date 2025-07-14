52879685c059e2090185be0b43ad2c45e6c49129
bakurvik <v.kurbatskaia@gmail.com>
2025-07-14 18:28:52 +0300

    add author message

--- models.go
+++ models.go
@@ -3,3 +3,9 @@ package common
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
+
+type AuthorMessage struct {
+	ID       string `json:"id"`
+	FullName string `json:"full_name"`
+	Action   string `json:"action"`
+}
